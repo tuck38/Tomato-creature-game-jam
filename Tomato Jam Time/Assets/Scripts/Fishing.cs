ca40697044cd3b0a4693f13646d514e6b85fa7d2
greembeems <hea4002@g.rit.edu>
2022-06-15 22:14:12 -0400

    Fishing works, you should time out as well

--- Tomato Jam Time/Assets/Scripts/Fishing.cs
+++ Tomato Jam Time/Assets/Scripts/Fishing.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Fishing : MonoBehaviour
 {
-
+    // Fishing
     [SerializeField] Casting casting;
     bool isCast = false;
     bool fishOn = false;
+
+    // Scoring nonsense
+    [SerializeField] Text scoring;
+    [SerializeField] Text timeOnScreen;
+    int score = 0;
+    float time = 1200;
+    float fishTime = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +27,33 @@ public class Fishing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (time > 0)
+        {
+            // if fishTime has passed 
+            if (fishTime <= 0)
+            {
+                // & your rod has been cast
+                if (isCast == true)
+                {
+                    // Change the score
+                    score++;
+                }
+
+                // Pick a random time
+                fishTime = Random.Range(15, 30);
+                Debug.Log(fishTime);
+            }
+
+            fishTime -= Time.deltaTime;
+            time -= Time.deltaTime;
+        }
+        else
+        {
+            SceneManager.LoadScene(2);
+        }
+
+        scoring.text = score.ToString();
+        timeOnScreen.text = ((int)time).ToString();
     }
 
     //check if the user has pressed on the rod ;)
